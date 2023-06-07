67bd08584389352efed4cf8e9ccee90568803407
Imad Bin Asad <105483337+imad10902@users.noreply.github.com>
2023-06-07 22:37:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import { useSpeechSynthesis } from "react-speech-kit";
